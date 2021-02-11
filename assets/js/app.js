a081474d2dec9095e9ba79f2370826797ba9f1ea
Sean Melody <seandmelody@gmail.com>
2021-02-10 20:50:58 -0800

    Arya

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+console.log("connected")
+
+const aryaImg = document.querySelectorAll(".aryaImg");
+
+const imgOptions = {
+    threshold: 1,
+}
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            console.log("Arya Image Intersecting!");
+            const dataSrc = entry.target.getAttribute("data-src");
+            entry.target.src = dataSrc;
+            imgObserver.unobserve(entry.target)
+        }
+    }, imgOptions);
+
+})
+
+aryaImg.forEach((image) => {
+    imgObserver.observe(image)
+})
